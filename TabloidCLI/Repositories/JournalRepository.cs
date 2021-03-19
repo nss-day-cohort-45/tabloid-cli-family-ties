fa76bbd0071074b0ed4c642601a2a12fcaec5cbc
Tim Curtis <timcurtis97@yahoo.com>
2021-03-19 11:24:49 -0500

    commit

---
+++ TabloidCLI/Repositories/JournalRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Data.SqlClient;
+using TabloidCLI.Models;
+
+namespace TabloidCLI.Repositories
+{
+    class JournalRepository : DatabaseConnector
+    {
+
+    }
+}
